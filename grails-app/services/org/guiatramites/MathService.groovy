6bf6eb5e65e8d834222e4c5f17da4a6f719d3d8c
Ramiro Martin <ramiro.martin@gmail.com>
2013-08-04 07:16:00 -0300

    Cambio matematica

--- grails-app/services/org/guiatramites/MathService.groovy
+++ grails-app/services/org/guiatramites/MathService.groovy
@@ -5,8 +5,7 @@ import static java.lang.Math.*
 class MathService {
 
     def distance(lat1, lat2, lon1, lon2) {
-        lat1+lon1 - (lat2+lon2)
-
+        sqrt(pow((lat1-lat2),2) + pow((lon1-lon2),2))
     }
 
     def findNearest(lat, lon) {
